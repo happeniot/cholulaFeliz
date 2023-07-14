731ce0c951f7b5b3a3fb0c286cbbb769064c523a
asd1495 <armando.salcedo95@gmail.com>
2023-07-13 18:24:15 -0600

    navbar now closes after clicking anchor

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let toggleBtn = document.querySelector('.toggle_btn')
 let toggleBtnIcon = document.querySelector('.toggle_btn i')
 let dropDownMenu = document.querySelector('.dropdown_menu')
+let dropdownLinks = document.querySelectorAll('.dropdown_menu a');
+
+dropdownLinks.forEach(function(link) {
+  link.addEventListener('click', function() {
+    dropDownMenu.classList.remove('open');
+    toggleBtnIcon.classList = 'fa-solid fa-bars';
+  });
+});
 
 toggleBtn.onclick = function() {
   dropDownMenu.classList.toggle('open')
